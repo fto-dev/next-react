acc447df389005ee586dae35186abb8ca6aa99b7
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2023-01-12 22:44:00 +0300

    default styles removed, layout, navbar and footer added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main >
         <div> TEST </div>
       </main>
     </>
