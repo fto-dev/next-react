ffe548104bd14cc6926c0073a36671fcea8c309d
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2023-01-15 16:46:21 +0300

    tailwind init

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import Layout from '../components/layout.js'
 import { appWithTranslation } from 'next-i18next';
 import nextI18NextConfig from '../next-i18next.config.js';
 
+import '../styles/globals.css'
+
 function App({ Component, pageProps }) {
   return (
     <Layout>
