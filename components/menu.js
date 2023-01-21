9b62a590ee0e4bbc504f870a6a97e4de03e0a03c
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2023-01-21 22:06:34 +0300

    menu hook added

---
+++ components/menu.js
@@ -0,0 +1,32 @@
+const GetMenu = () => {
+
+  const menuItemCollect = [];
+  process.env.localesFiles.map((page, index) => {
+      // Filter common translation file
+      if(page != 'common'){
+        let item = {};
+        if (page == 'index'){
+          item.index = "0";
+          item.name = page;
+          menuItemCollect.push( item );
+        } else if (page == 'blog'){
+          item.index = "1";
+          item.name = page;
+          menuItemCollect.push( item );
+        } else if (page == 'contact'){
+          item.index = "2";
+          item.name = page;
+          menuItemCollect.push( item );
+        }
+      }
+  })
+
+  menuItemCollect.sort(function(a, b) {
+    // reorder all menus manuel index number
+    return a.index - b.index;
+  });
+
+  return menuItemCollect;
+};
+
+export default GetMenu;
