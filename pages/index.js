9e5c5fa0e4c715846945a45eb7e9c55f8a36baf8
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2023-02-04 02:08:15 +0300

    homepage banner style image change

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,46 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import nextI18NextConfig from '../next-i18next.config'
-import { useTranslation } from 'next-i18next';
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import nextI18NextConfig from "../next-i18next.config";
+import { useTranslation } from "next-i18next";
+import { useState } from "react";
 
-import MultipleContent from '/components/multiple-content.js'
+//@todo: @ source alias add
+import MultipleContent from "/components/multiple-content.js";
 
+import { StringMerge } from "../hooks/stringMerge";
+import { useEffect } from "react";
 
-export async function getStaticProps({ locale }){
+import image from '../public/images/banner-wide.jpeg'
+
+export async function getStaticProps({ locale }) {
   return {
     props: {
-        ...(await serverSideTranslations(locale, process.env.localesFiles, nextI18NextConfig ))
-    }
-  }
+      ...(await serverSideTranslations(
+        locale,
+        process.env.localesFiles,
+        nextI18NextConfig
+      )),
+    },
+  };
 }
 
 export default function Home() {
-
   const { t } = useTranslation(process.env.localesFiles);
   const router = useRouter();
-  const homeContent = t('homeContent', { returnObjects: true, ns: router.localeFile });
-  
+  const homeContent = t("homeContent", {
+    returnObjects: true,
+    ns: router.localeFile,
+  });
+
+  const stringExample = "Name";
+  const stringExampleSur = "Surname";
+
+  let dant = StringMerge();
+
+  const styling = { backgroundImage: `url(${image.src})`}
+
   return (
     <>
       <Head>
@@ -30,42 +49,37 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='mt-5'>
-        <div className="container mx-auto px-5">
-          
-          <div className='text-3xl font-bold underline'> { t('homeHeading', { ns: router.localeFile }) } </div> 
+      <>
+        {homeContent.map((item, index) => (
+          <div key={index}>
+            <div className="hero is-medium hero-learn" style={styling}>
+              <div className="hero-body"></div>
+              <div className="hero-footer">
+                <h1 className="title is-1 has-text-centered" >
+                  <span className="has-background-white"> {item.title}</span>
+                </h1>
+              </div>
+            </div>
 
-          <div>
-            <ul className="list-none mt-3">
-              {
-                  homeContent.map((item , index) => (
-                    <li className={index == 0 ? '' : 'mt-5'} key={index}>
-                        <b className='mb-5'>{item.title}</b>
-                        <br></br>
-                        {item.description}
-                      { 
-                        item.image.map((subItem , subIndex) => (
-                          <div className='mt-5' key={subIndex}>
-                            <Image
-                              src={subItem.src}
-                              alt={subItem.alt}
-                              width={360}
-                              height={540}
-                              
-                            />
-                          </div>
-                        ))
-                      }
-                    </li>
-                  ))
-              }
-            </ul>
+            <section className="section_ ">
+              <div className="section">
+                <div className="container">
+                  <p className="is-size-5">{item.description}</p>
+                  <div className="spacer-s"></div>
+                  <div className="spacer-m"></div>
+                </div>
+              </div>
+            </section>
           </div>
-          <div>
-            <MultipleContent/>
-          </div>
-        </div>  
-      </main>
+        ))}
+
+        {/* 
+        @todo: add interactivity, user can change content with info promp (localstorage update)
+        <div className="text-3xl font-bold underline"> {dant} </div>
+        <div className="text-3xl font-bold underline"> {t("homeHeading", { ns: router.localeFile })} </div> */}
+        
+        
+      </>
     </>
-  )
+  );
 }
